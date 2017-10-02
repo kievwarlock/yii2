fc56e05406e9261ad77a3ba29993cb1c7587c3e7
kievwarlock <kievwarlock@gmail.com>
2017-10-02 16:31:08 +0300

    Test Test

--- views/site/index.php
+++ views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations! Hellos23 22</h1>
+        <h1>Congratulations! Hellos23 asdasd </h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
